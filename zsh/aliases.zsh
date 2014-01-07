85e5dd0f99d05fad1d8e29db0cd932f261054432
Adam Dawkins <adamdawkins@gmail.com>
2014-01-07 01:11:11 +0000

    Setup .dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,4 @@
 alias reload!='. $HOME/.zshrc'
-alias a='ack'
 alias ..='cd ..'
 alias g='git'
 alias s='gs'
-alias kit='vim ~/Google\ Drive/wolves/2014/admin/jerseys.txt'
-alias playonui='cd ~/development/playon-ui'
-alias playonapp='cd ~/code/rails/playon'
-
