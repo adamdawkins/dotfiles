5f8b90df8bcf24521bdb9e848a0f1e79ab3d4279
Adam Dawkins <adamdawkins@gmail.com>
2015-06-09 22:12:55 +0100

    Latest .dotfiles with zsh

--- projects/functions.zsh
+++ projects/functions.zsh
@@ -6,8 +6,25 @@ function mvhq () {
   cd ~/development/mint-velvet/applications/mvhq/$1
 }
 
+
 function fashion () {
   cd ~/development/newin/myfashioninbox2
+}
+
+function ape () {
+  cd ~/development/mint-velvet/projects/mailchimp/apebuilder
+}
+
+function engage () {
+  cd ~/development/mint-velvet/applications/mint-engage
+}
+
+function outfits () {
+  cd ~/development/mint-velvet/applications/outfits
+}
+
+function spacetalk () {
+  cd ~/code/meteor/SpaceTalk
 }
 
 function ape () {
