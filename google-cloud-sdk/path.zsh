4ca5f11ed5f737460729ce94faf4d6518cef89db
Adam Dawkins <adamdawkins@gmail.com>
2015-06-24 20:25:01 +0100

    Add google cloud sdk to source

---
+++ google-cloud-sdk/path.zsh
@@ -0,0 +1,6 @@
+
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/adamdawkins/development/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables zsh completion for gcloud.
+source '/Users/adamdawkins/development/google-cloud-sdk/completion.zsh.inc'
