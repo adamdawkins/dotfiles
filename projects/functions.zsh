46a7ac7a2a20432a7a804daab48d302eaa839f3c
Adam Dawkins <adamdawkins@gmail.com>
2016-03-31 20:51:11 +0100

    Update

--- projects/functions.zsh
+++ projects/functions.zsh
@@ -6,10 +6,10 @@ function mvhq () {
   cd ~/development/mint-velvet/applications/mvhq/$1
 }
 
-
-function fashion () {
-  cd ~/development/newin/myfashioninbox2
+function engage () {
+  cd ~/development/mint-velvet/applications/mint-engage
 }
+
 
 function ape () {
   cd ~/development/mint-velvet/projects/mailchimp/apebuilder
@@ -23,40 +23,6 @@ function outfits () {
   cd ~/development/mint-velvet/applications/outfits
 }
 
-function spacetalk () {
-  cd ~/code/meteor/SpaceTalk
-}
-
-function ape () {
-  cd ~/development/mint-velvet/projects/mailchimp/apebuilder
-}
-
-function engage () {
-  cd ~/development/mint-velvet/applications/mint-engage
-}
-
-function outfits () {
-  cd ~/development/mint-velvet/applications/outfits
-}
-
-function spacetalk () {
-  cd ~/code/meteor/SpaceTalk
-}
-
-function zoom_image () {
-# typeset -a product_ids
-  prefix="http://www.mintvelvet.co.uk/pws/client/images/responsive/catalogue/$1/1200px/"
-  wget ${prefix}$1.jpg
-  for i in {1..3}
-  do
-    wget ${prefix}$1_${i}.jpg
-  done
-}
-
-function proto () {
-  cd ~/code/meteor/prototyping
-}
-
-function focuster () {
-  cd ~/development/focuster
+function rebound () {
+ cd ~/dragondrop/dev/rebound/mvp
 }
