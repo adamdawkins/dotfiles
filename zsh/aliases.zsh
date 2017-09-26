f0cb50f3f71c2ae1496ca91260e42e44215f330f
Adam Dawkins <adam@dragondrop.uk>
2017-09-26 16:05:24 +0100

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias ..='cd ..'
 alias g='git'
 alias s='gs'
 alias nom='npm cache clean && rm -rf node_modules && mkdir node_modules && touch node_modules/.metadata_never_index && npm install'
+
+alias o='open'
