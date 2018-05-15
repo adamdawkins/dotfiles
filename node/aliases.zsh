6c9003ff749feb4ec05bece845b31e254d18fc88
Adam Dawkins <adam@dragondrop.uk>
2018-05-15 22:12:15 +0100

    Add 'trash_node_modules' alias

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,1 +1,3 @@
 alias scripts='jq .scripts package.json'
+# removes node modules from all folders in this directory
+alias trash_node_modules='find . -maxdepth 2 -name "node_modules" -exec rm -rf '{}' \;'
