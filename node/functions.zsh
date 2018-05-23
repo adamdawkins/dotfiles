9f91d33eb636d29d1d78dcf5ea247eca8bc000c4
Adam Dawkins <adam@dragondrop.uk>
2018-05-23 21:39:56 +0100

    Update bpm

--- node/functions.zsh
+++ node/functions.zsh
@@ -1,19 +1,24 @@
+# help text when just running `bpm`
+help="
+Usage: bpm <command> <flag> <packages>
+
+where <command> is one of:
+    a, add, i, install,
+    r, remove, u, uninstall
+
+and where <flag> is one of:
+    -h, -d, -g
+
+and where <packages> is one or many npm package names
+
+bpm add|a|i|install       npm install --save
+bpm i -d                  npm install --save-dev
+bpm remove|r|u|uninstall  npm uninstall --save
+bpm u -d                  npm uninstall --save-dev
+bpm <package>             npm run <package>
+"
+
 bpm() {
-	help="
-	Usage: bpm <command>
-
-	where <command> is one of:
-	a, add, d, dev, i, install,
-	r, remove, u, uninstall
-
-	or where <command> is a task you would like to run
-
-	bpm add|a|i|install       npm install --save
-	bpm dev|d                 npm install --save-dev
-	bpm remove|r|u|uninstall  npm uninstall --save
-	bpm <command>             npm run <command>
-	"
-
 	cmd=""
 
 	if [ -f .meteor/packages ]; then
@@ -23,34 +28,51 @@ bpm() {
 	fi
 
 	cmd="$cmd npm"
+	# scope will be --save --save-dev or -g
+	scope="--save"
+	# action will be one of install or uninstall
+	action=""
+	# packages will be the list of npm package names
+	packages=""
 
-	case "$1" in
+	# $0 is all the parameters provided after the bpm command
+	for argument in $@
+	do
+		# The conditions
+		case $argument in
+			-h) echo "${help}" ;;
+			-d) scope="--save-dev" ;;
+			-g) scope="-g" ;;
+			add|a|i|install) action="install" ;;
+			remove|r|u|uninstall) action="uninstall" ;;
+			# for all other arguments, add to the list of packages
+			*) packages="$packages $argument"
+		esac
+	done
 
-		add|a|i|install)
-			cmd="$cmd install --save $2"
-		;;
+	# if the command contains packages, we can run or (un)install them
+	if [ "$packages" != "" ]; then
+		# if there's an action, use that, otherwise we'll run the package
+		if [ "$action" != "" ]; then
+			cmd="$cmd $action"
 
-		remove|r|u|uninstall)
-			cmd="$cmd uninstall --save $2"
-		;;
-
-		dev|d)
-			cmd="$cmd install --save-dev $2"
-		;;
-
-		# default
-		*)
-			if [ "$1" = "" ]; then
-				echo "${help}"
-			else
-				cmd="$cmd run $1"
+			# we only care about scope if an action other than run is involved
+			if [ "$scope" != "" ]; then
+				cmd="$cmd $scope"
 			fi
-		;;
+		else
+			cmd="$cmd run"
+		fi
 
-    esac
+		cmd="$cmd $packages"
 
-	if [ "$1" != "" ]; then
 		echo $cmd
 		eval $cmd
+	# special exception for when you want to just run `bpm i` and nothing else
+	elif [ "$action" == "install" ]; then
+		echo "npm i"
+		eval "npm i"
+	else
+		echo "${help}"
 	fi
 }
