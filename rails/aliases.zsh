4383a142342c7870dfbb8beea7184de390fbed12
Adam Dawkins <adam@dragondrop.uk>
2018-09-20 10:12:28 +0100

    ✅ Add 'killrails' alias

--- rails/aliases.zsh
+++ rails/aliases.zsh
@@ -4,3 +4,4 @@ alias warren='rails' # warren g migration
 alias drake='rake' # drake db:migrate
 alias mel='rails' # mel c
 alias canIsayyes='rails' # canIsayyes s
+alias killrails='kill -9 $(cat ./tmp/pids/server.pid)'
