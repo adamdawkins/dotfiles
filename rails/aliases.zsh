d24137e5ebfefa8b41cb70e1c74987e8664ebb63
Adam Dawkins <adam@dragondrop.uk>
2020-01-20 22:05:30 +0000

    Add `bundle exec` to all rails alias commands

--- rails/aliases.zsh
+++ rails/aliases.zsh
@@ -1,11 +1,11 @@
-alias r='rails'
+alias r='bundle exec rails'
 alias b='bundle exec'
 alias bi='bundle install'
 alias bu='bundle update'
 alias guard='bundle exec guard'
 
-alias warren='rails' # warren g migration
-alias drake='rake' # drake db:migrate
-alias mel='rails' # mel c
-alias canIsayyes='rails' # canIsayyes s
+alias warren='bundle exec rails' # warren g migration
+alias drake='bundle exec rake' # drake db:migrate
+alias mel='bundle exec rails' # mel c
+alias canIsayyes='bundle exec rails' # canIsayyes s
 alias killrails='kill -9 $(cat ./tmp/pids/server.pid)'
