209c40ad7dda624b5cc5290b7533ea7c512aacfd
Adam Dawkins <adam@dragondrop.uk>
2020-01-20 22:06:43 +0000

    Add grso=git remote show origin

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,6 +21,7 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias grh='git reset HEAD'
+alias grso='git remote show origin'
 alias feature='git flow feature'
 alias release='git flow release'
 alias hotfix='git flow hotfix'
