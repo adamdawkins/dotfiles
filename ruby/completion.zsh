c053b9f4259838d12e4550a04d84e5dce83bcab3
Adam Dawkins <adam@dragondrop.uk>
2020-02-11 20:54:25 +0000

    Ruby changes

--- ruby/completion.zsh
+++ ruby/completion.zsh
@@ -1,26 +1,26 @@
-# Stolen from
-#   https://github.com/sstephenson/rbenv/blob/master/completions/rbenv.zsh
-
-if [[ ! -o interactive ]]; then
-    return
-fi
-
-compctl -K _rbenv rbenv
-
-_rbenv() {
-  local word words completions
-  read -cA words
-  word="${words[2]}"
-
-  if [ "${#words}" -eq 2 ]; then
-    completions="$(rbenv commands)"
-  else
-    completions="$(rbenv completions "${word}")"
-  fi
-
-  reply=("${(ps:\n:)completions}")
-}
-
-if type brew &>/dev/null; then
-	FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
-fi
+# # Stolen from
+# #   https://github.com/sstephenson/rbenv/blob/master/completions/rbenv.zsh
+#
+# if [[ ! -o interactive ]]; then
+#     return
+# fi
+#
+# compctl -K _rbenv rbenv
+#
+# _rbenv() {
+#   local word words completions
+#   read -cA words
+#   word="${words[2]}"
+#
+#   if [ "${#words}" -eq 2 ]; then
+#     completions="$(rbenv commands)"
+#   else
+#     completions="$(rbenv completions "${word}")"
+#   fi
+#
+#   reply=("${(ps:\n:)completions}")
+# }
+#
+# if type brew &>/dev/null; then
+# 	FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+# fi
--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,5 +1,6 @@
-export PATH="/usr/local/opt/openssl/bin:$PATH"
-
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
-export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+# export PATH="/usr/local/opt/openssl/bin:$PATH"
+#
+# export LDFLAGS="-L/usr/local/opt/openssl/lib"
+# export CPPFLAGS="-I/usr/local/opt/openssl/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+export PATH="~/.rbenv/bin:~/.rbenv/shims:$PATH"
